b3ac856735e2fce0e792fdbaeddc7b9ac7908ec3
Washington Antunes <washington.cead@gmail.com>
2023-03-26 12:52:51 -0300

    implementação teste choice

--- src/main/java/br/com/wti/school/persistence/model/Choice.java
+++ src/main/java/br/com/wti/school/persistence/model/Choice.java
@@ -16,18 +16,64 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Choice extends AbstractEntity{
+public class Choice extends AbstractEntity {
 
 	private static final long serialVersionUID = -5415438648913158113L;
-	
+
 	@NotEmpty(message = "The field title cannot be empty")
-    @ApiModelProperty(notes = "The title of the choice")
-    private String title;
-    @NotNull(message = "The field correctAnswer must be true or false")
-    @ApiModelProperty(notes = "Correct answer for the associated question, you can have only one correct answer per question")
-    private boolean correctAnswer;
-    @ManyToOne(optional = false)
-    private Question question;
-    @ManyToOne(optional = false)
-    private Professor professor;
+	@ApiModelProperty(notes = "The title of the choice")
+	private String title;
+	@NotNull(message = "The field correctAnswer must be true or false")
+	@ApiModelProperty(notes = "Correct answer for the associated question, you can have only one correct answer per question")
+	private boolean correctAnswer;
+	@ManyToOne(optional = false)
+	private Question question;
+	@ManyToOne(optional = false)
+	private Professor professor;
+
+	public static final class Builder {
+		private Choice choice;
+
+		private Builder() {
+			choice = new Choice();
+		}
+
+		public static Builder newChoice() {
+			return new Builder();
+		}
+
+		public Builder id(Long id) {
+			choice.setId(id);
+			return this;
+		}
+
+		public Builder enabled(boolean enabled) {
+			choice.setEnabled(enabled);
+			return this;
+		}
+
+		public Builder title(String title) {
+			choice.setTitle(title);
+			return this;
+		}
+
+		public Builder correctAnswer(boolean correctAnswer) {
+			choice.setCorrectAnswer(correctAnswer);
+			return this;
+		}
+
+		public Builder question(Question question) {
+			choice.setQuestion(question);
+			return this;
+		}
+
+		public Builder professor(Professor professor) {
+			choice.setProfessor(professor);
+			return this;
+		}
+
+		public Choice build() {
+			return choice;
+		}
+	}
 }
